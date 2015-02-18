e44f596d63512ff3806ab09c56a478bd6ed4a930
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-18 11:03:28 -0800

    Working paperclip for photo of the day!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,19 @@
 class HomeController < ApplicationController
+  def new
+  	@photo = Photo.new
+  end
+
   def create
+  	@photo = Photo.create(name:params[:photo][:name], image:params[:photo][:image], description:params[:photo][:description])
+  	if @photo.save
+  		redirect_to home_index_path
+  	else 
+  		flash[:errors] = @photo.errors.full_messages
+  		redirect_to root_path
+  	end
   end
 
   def index
+  	@photo = Photo.last
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,4 @@
 class Photo < ActiveRecord::Base
 	has_attached_file :image
-	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
 end
